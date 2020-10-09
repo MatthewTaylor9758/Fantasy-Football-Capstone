cdc0967a763fc8c6f23893faf4315d6006669771
Matthew Taylor <matthewtaylor9758@gmail.com>
2020-10-08 21:11:17 -0400

    add models that might work

--- flask_react_starter/starter_app/app/models.py
+++ flask_react_starter/starter_app/app/models.py
@@ -12,6 +12,9 @@ class User(db.Model):
   email = db.Column(db.String(255), nullable = False, unique = True)
   hashed_password = db.Column(db.String(100), nullable = False)
 
+  # Set up many-to-one -> many-teams
+  # Set up many-to-one -> many-leagues POSSIBLY
+
   def to_dict(self):
     return {
       "id": self.id,
@@ -31,3 +34,43 @@ class User(db.Model):
 
   def check_password(self, password):
     return check_password_hash(self.password, password)
+
+class League(db.Model):
+  __tablename__ = 'leagues'
+
+  id = db.Column(db.Integer, nullable = False,  primary_key = True)
+  name = db.Column(db.String(100), nullable = False)
+  commish_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+  # Set up many-to-one -> many-teams
+
+class Team(db.Model):
+  __tablename__ = 'teams'
+
+  id = db.Column(db.Integer, nullable = False,  primary_key = True)
+  name = db.Column(db.String(100), nullable = False)
+  owner_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+  league_id = db.Column(db.Integer, db.ForeignKey('leagues.id'))
+
+  # Set up many-to-one -> many-FFSplayers
+
+class Player(db.Model):
+  __tablename__ = 'players'
+
+  id = db.Column(db.Integer, nullable = False,  primary_key = True)
+  full_name = db.Column(db.String(100), nullable = False, unique = True)
+  first_name = db.Column(db.String(50), nullable = False)
+  last_name = db.Column(db.String(50), nullable = False)
+  nfl_team = db.Column(db.String(100), nullable = False)
+  position = db.Column(db.String(5), nullable = False)
+  height = db.Column(db.String(10), nullable = False)
+  weight = db.Column(db.String(10), nullable = False)
+  dob = db.Column(db.Date, nullable = False)
+  college = db.Column(db.String(100), nullable = False)
+
+class FFSplayer(db.Model):
+  __tablename__ = 'ffsplayers'
+
+  id = db.Column(db.Integer, nullable = False,  primary_key = True)
+  player_id = db.Column(db.Integer, db.ForeignKey('players.id'))
+  team_id = db.Column(db.Integer, db.ForeignKey('teams.id'))
