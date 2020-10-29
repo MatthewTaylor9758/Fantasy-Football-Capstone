3862a37b93a19b8795e3f3121c1de8d38001bb0f
Matthew Taylor <matthewtaylor9758@gmail.com>
2020-10-29 16:42:41 -0400

    get app ready for heroku deployment

---
+++ flask_react_starter/starter_app/app/static/precache-manifest.3acb16863deaebc9c4f73e484760feb5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dea8e3f14122e28a9cfbb58781ea2f8b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e0a8123b935db7d0434",
+    "url": "/static/css/main.5f9b8a44.chunk.css"
+  },
+  {
+    "revision": "2df6841e21efbdd10f9d",
+    "url": "/static/js/2.208833f8.chunk.js"
+  },
+  {
+    "revision": "8a9a2ae4a25f202f52818f8f8074424e",
+    "url": "/static/js/2.208833f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e0a8123b935db7d0434",
+    "url": "/static/js/main.cb34077e.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "b0dc9ae557cac5b8753575df5f55cc33",
+    "url": "/static/media/data_image.b0dc9ae5.jpg"
+  },
+  {
+    "revision": "a1d9a169e5b1e78535b0db905fc44943",
+    "url": "/static/media/hiclipart_football.a1d9a169.png"
+  },
+  {
+    "revision": "44c48b3718cf08dd127ac535a29f7807",
+    "url": "/static/media/players_background.44c48b37.jpg"
+  },
+  {
+    "revision": "6d50b6ac8fc6d65b612b2b752c71696e",
+    "url": "/static/media/stats_picture.6d50b6ac.png"
+  }
+]);
